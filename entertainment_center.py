5809a2c3a7cbf6151278ae7cb4db5b2713d38c8e
Andy Hsiang <xenogt@gmail.com>
2017-06-29 18:53:37 -0400

    added 9 movies

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,10 +1,44 @@
 import fresh_tomatoes
 import media
 
-toy_story = media.Movie(
-    'Toy Story',
-    'http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg',
-    'https://www.youtube.com/watch?v=vwyZH85NQC4')
+transformer = media.Movie(
+    'Transformer',
+    'https://image.tmdb.org/t/p/w640/f8Ng1Sgb3VLiSwAvrfKeQPzvlfr.jpg',
+    'https://www.youtube.com/watch?v=6Vtf0MszgP8')
+beauty_and_beast = media.Movie(
+    'Beauty And The Beast',
+    'https://image.tmdb.org/t/p/w640/tWqifoYuwLETmmasnGHO7xBjEtt.jpg',
+    'https://www.youtube.com/watch?v=5PSNL1qE6VY')
+despicable_me = media.Movie(
+    'Despicable Me 3',
+    'https://image.tmdb.org/t/p/w640/5qcUGqWoWhEsoQwNUrtf3y3fcWn.jpg',
+    'https://www.youtube.com/watch?v=euz-KBBfAAo')
+lord_of_ring = media.Movie(
+    'Lord of the Rings',
+    'https://image.tmdb.org/t/p/w640/6oom5QYQ2yQTMJIbnvbkBL9cHo6.jpg',
+    'https://www.youtube.com/watch?v=V75dMMIW2B4')
+harry_potter = media.Movie(
+    'Harry Potter',
+    'https://image.tmdb.org/t/p/w640/dCtFvscYcXQKTNvyyaQr2g2UacJ.jpg',
+    'https://www.youtube.com/watch?v=dtiklALGz20')
+hunger_games = media.Movie(
+    'Hunger Games',
+    'http://upload.wikimedia.org/wikipedia/en/4/42/HungerGamesPoster.jpg',
+    'https://www.youtube.com/watch?v=PbA63a7H0bo')
+wonder_woman = media.Movie(
+    'Wonder Woman',
+    'https://image.tmdb.org/t/p/w640/gfJGlDaHuWimErCr5Ql0I8x9QSy.jpg',
+    'https://www.youtube.com/watch?v=INLzqh7rZ-U')
+warcraft = media.Movie(
+    'Warcraft',
+    'https://image.tmdb.org/t/p/w640/ckrTPz6FZ35L5ybjqvkLWzzSLO7.jpg',
+    'https://www.youtube.com/watch?v=RhFMIRuHAL4')
+the_foreigner = media.Movie(
+    'The Foreigner',
+    'https://image.tmdb.org/t/p/w640/mZ6G8LorvBJ9LdzE2XhePLUtWpy.jpg',
+    'https://www.youtube.com/watch?v=LmImJ6ZUiqE'
+)
 
-movies = [toy_story]
+movies = [transformer, beauty_and_beast, despicable_me, hunger_games, lord_of_ring, harry_potter, wonder_woman, warcraft, the_foreigner]
+
 fresh_tomatoes.open_movies_page(movies)
--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -107,7 +107,7 @@ main_page_content = '''
       <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
         <div class="container">
           <div class="navbar-header">
-            <a class="navbar-brand" href="#">Fresh Tomatoes Movie Trailers</a>
+            <a class="navbar-brand" href="#">Andy's Favorite Movies</a>
           </div>
         </div>
       </div>
